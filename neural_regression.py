27fa05b3ac28400b5e8f3e4a0b6e1e116bd0b8d2
Dan Scarafoni <dan@scarafoni.com>
2014-12-24 19:12:25 -0800

    remove print output

--- neural_regression.py
+++ neural_regression.py
@@ -1,28 +1,37 @@
-"train a regression MLP"
+"""runs iterations to train a neural network"""
+
 import numpy as np
 import cPickle as pickle
 from math import sqrt
 from pybrain.datasets.supervised import SupervisedDataSet as SDS
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
+
 train_file = 'data/train.csv'
 validation_file = 'data/validation.csv'
-output_model_file = 'model.pkl'
-hidden_size = 100
-epochs = 600
+hidden_size = 50
+epochs = 5
+
 # load data
 train = np.loadtxt( train_file, delimiter = ',' )
 validation = np.loadtxt( validation_file, delimiter = ',' )
 train = np.vstack(( train, validation ))
 x_train = train[:,0:-1]
+print('x_train', x_train)
 y_train = train[:,-1]
+print('y_train', y_train)
 y_train = y_train.reshape( -1, 1 )
+print('y_train2', y_train)
 input_size = x_train.shape[1]
+print('input_size', input_size)
 target_size = y_train.shape[1]
+print('target_size', target_size)
+
 # prepare dataset
 ds = SDS( input_size, target_size )
 ds.setField( 'input', x_train )
 ds.setField( 'target', y_train )
+
 # init and train
 net = buildNetwork( input_size, hidden_size, target_size, bias = True )
 trainer = BackpropTrainer( net,ds )
@@ -31,5 +40,3 @@ for i in range( epochs ):
     mse = trainer.train()
     rmse = sqrt( mse )
     print "training RMSE, epoch {}: {}".format( i + 1, rmse )
-
-pickle.dump( net, open( output_model_file, 'wb' ))
